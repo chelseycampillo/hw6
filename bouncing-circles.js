c8cf0d4cc7f225e10a1646784e4219443a0ba89c
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-25 20:14:05 -0800

    Create bouncing-circles.js

---
+++ bouncing-circles.js
@@ -0,0 +1,56 @@
+//the radius of the circle decreases every time the circle hits and edge of the canvas
+
+var circles = [];
+
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB);
+
+  for (var index = 0; index < 100; index = index + 1) {
+    // new "circle" object, with x, y, xd, yd, and c properties:
+    circles[index] = {
+      x: width / 2,
+      y: height / 2,
+      xd: random(-2, 2),
+      yd: random(-2, 2),
+      c: color(random(360), 60, 100),
+      r: 20
+    }
+  }
+}
+
+function draw() {
+  background(0);
+  noStroke();
+  
+  for (var index = 0; index < 100; index = index + 1) {
+    // get circle object
+    var circle = circles[index];
+
+    // draw it
+    //variables don't mean anything to the computer...
+    //the only reason the variable "x" is denoted as the x coordinate is because you placed the x variable in the x location of the ellipse
+    fill(circle.c);
+    ellipse(circle.x, circle.y, circle.r);
+
+    // move it according to direction
+    circle.x = circle.x + circle.xd;
+    circle.y = circle.y + circle.yd;
+
+    // check boundaries and update directions
+    if (circle.x > width || circle.x < 0) { 
+        circle.r -= 5
+      	circle.xd = -circle.xd;
+      	//resets size of circle when it disappears
+      	 if (circle.r = 1) {
+      			circle.r = 20;
+    			}
+      
+    }
+    if (circle.y > height || circle.y < 0) {
+      circle.r -= 5;
+      circle.yd = -circle.yd;
+    }
+  }
+}
+
